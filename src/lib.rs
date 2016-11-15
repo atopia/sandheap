77b57a0b93c9a94b0444def75725457a25379f75
Benjamin Lamowski <benjamin@lamowski.net>
2016-11-15 17:00:41 +0100

    call unshare for a start

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
+extern crate nix;
+use nix::sched::*;
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
+    }
+}
+
+pub fn sandheap_setup() {
+    match unshare(CLONE_FILES | CLONE_FS | CLONE_NEWIPC | CLONE_NEWNET | CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWUSER | CLONE_NEWUTS) {
+        Ok(_) => println!("set up sandheap"),
+        Err(e) => println!("unshare failed with error {}", e)
     }
 }
