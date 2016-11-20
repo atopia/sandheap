f653522f023f6f8338471de35e93c8dd0a06d1d2
Benjamin Lamowski <benjamin@lamowski.net>
2016-11-20 16:56:06 +0100

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,13 @@ use nix::sched::*;
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }
 
 pub fn setup() {
-    match unshare(CLONE_FILES | CLONE_FS | CLONE_NEWIPC | CLONE_NEWNET | CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWUSER | CLONE_NEWUTS) {
+    match unshare(CLONE_FILES | CLONE_FS | CLONE_NEWIPC | CLONE_NEWNET | CLONE_NEWNS |
+                  CLONE_NEWPID | CLONE_NEWUSER | CLONE_NEWUTS) {
         Ok(_) => println!("sandheap: set up"),
-        Err(e) => println!("sandheap: unshare failed with error {}", e)
+        Err(e) => println!("sandheap: unshare failed with error {}", e),
     }
 }
