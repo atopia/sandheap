1174b74ff2cc33204b8ca62c81c7fb23987f7931
Benjamin Lamowski <benjamin@lamowski.net>
2017-03-19 23:38:30 +0100

    get pipe file descriptors for sandbox setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 extern crate nix;
 use nix::sched::*;
 
+use  std::os::unix::io::RawFd;
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {}
 }
 
-pub fn setup() {
+pub fn setup(file_in: RawFd, file_out: RawFd) {
     match unshare(CLONE_FILES | CLONE_FS | CLONE_NEWIPC | CLONE_NEWNET | CLONE_NEWNS |
                   CLONE_NEWPID | CLONE_NEWUSER | CLONE_NEWUTS) {
         Ok(_) => println!("sandheap: set up"),
